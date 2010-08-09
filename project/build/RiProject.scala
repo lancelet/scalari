ffcfa7a61506c8b783bc040b388dc3c44524b665
Jonathan Merritt <merritt@unimelb.edu.au>
2010-08-09 15:59:51 +1000

    Added ScalaTest and ScalaCheck to the project. - Additionally removed unchecked compiler option for the time being.

--- project/build/RiProject.scala
+++ project/build/RiProject.scala
@@ -1,5 +1,15 @@
 import sbt._
 
 class RiProject(info: ProjectInfo) extends DefaultProject(info) {
-  override def compileOptions = super.compileOptions ++ Seq(Unchecked)
+  // add unchecked to compiler options
+  //override def compileOptions = super.compileOptions ++ Seq(Unchecked)
+  
+  // ScalaTools repositories
+  val scalaToolsSnapshots = ScalaToolsSnapshots
+  
+  // ScalaTest
+  val scalaTest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.final-SNAPSHOT"
+  
+  // ScalaCheck
+  val scalaCheck = "org.scalacheck" % "scalacheck" % "1.5"
 }
