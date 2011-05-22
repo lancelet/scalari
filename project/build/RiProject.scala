0b5b90c4d7df601c431d2f273167a440c5ffe4a4
Jonathan Merritt <merritt@unimelb.edu.au>
2011-05-22 17:25:10 -0400

    Added sbteclipsify support.  An Eclipse project should now be creatable from sbt using the "eclipse" action.

--- project/build/RiProject.scala
+++ project/build/RiProject.scala
@@ -1,6 +1,9 @@
 import sbt._
+import de.element34.sbteclipsify._
 
-class RiProject(info: ProjectInfo) extends DefaultProject(info) {
+class RiProject(info: ProjectInfo) extends DefaultProject(info) 
+with Eclipsify {
+
   // add unchecked to compiler options
   //override def compileOptions = super.compileOptions ++ Seq(Unchecked)
   
@@ -8,7 +11,8 @@ class RiProject(info: ProjectInfo) extends DefaultProject(info) {
   val scalaToolsSnapshots = ScalaToolsSnapshots
   
   // ScalaTest
-  val scalaTest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.final-SNAPSHOT"
+  val scalaTest = "org.scalatest" % "scalatest" % 
+    "1.2-for-scala-2.8.0.final-SNAPSHOT"
   
   // ScalaCheck
   val scalaCheck = "org.scala-tools.testing" % "scalacheck_2.8.0" % "1.7"
